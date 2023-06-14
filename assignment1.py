d757440142e88d375820f355ad92e23cbc2c05d4
Nihabhagwat1109 <136458091+Nihabhagwat1109@users.noreply.github.com>
2023-06-15 00:07:30 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,5 @@
+marks1= int(input("enter marks 1:"))
+marks2= int(input("enter marks 2:"))
+marks3= int(input("enter marks 3:"))
+avg=(marks1+marks2+marks3)/3
+print("average of marks 1, marks 2, marks 3 is:", avg)
